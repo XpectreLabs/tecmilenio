d5081984a646d6e4949606e0ddf05d90260a3822
CrisEmyoi <157163248+CrisEmyoi@users.noreply.github.com>
2024-07-04 15:24:48 -0600

    Initial commit

---
+++ CristianEmir/types.js
@@ -0,0 +1,40 @@
+/* 
+console.log('It Works');
+
+const name = 'Jimmy';
+const age = 48;
+
+const hello = `hello my names is ${name} and i'm ${age} years old`;
+
+console.log(hello);*/
+
+
+/* Numeros
+
+const age = 52.5;
+const name = 'Erick';
+
+//const a = 10;
+//const b = 20;
+
+const smaties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smaties / kids);
+const dadGets = smaties % kids;
+console.log(`Each kid gets ${eachKidGets} Smarties, and Dad gets ${dadGets}`);
+*/
+/*
+const person = {
+  first: `wes`,
+  last: `bos`,
+  age: 48,
+};
+*/
+
+let isDrawing = false;
+let age = 18;
+const ofAge = age > 19;
+console.log(ofAge);
+
+age = 100;
+let age2 = 100;
